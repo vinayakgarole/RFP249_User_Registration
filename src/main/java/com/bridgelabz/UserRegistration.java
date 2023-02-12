aeec81822957d2805fe5d421eb6e785654d87d4a
vinayakgarole <vinayakgarole10@gamil.com>
2023-02-12 12:10:06 +0530

    Modified: Validate first name which start with cap and has minimum 3 characters

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,9 +1,25 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
-    public static void main(String[] args) {
+    public static final Scanner sc = new Scanner(System.in);
 
-        System.out.println("Welcome to User Registration Program");
+    public static void validFirstName() {
+        System.out.println("Enter the First Name: ");
+        String name = sc.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        boolean result = matcher.matches();
+
+        if (result) {
+            System.out.println("Enter name is valid");
+        } else {
+            System.out.println("Enter name is invalid");
+        }
     }
 }
---
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class UserRegistrationMain {
+
+    public static void main(String[] args) {
+
+        UserRegistration.validFirstName();
+    }
+}
